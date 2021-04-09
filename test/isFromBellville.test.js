c9c532c7ab449a36e6470df8d1d53b61bdbae0a8
josephmabusela <josephmabusela5@gmail.com>
2021-04-09 13:34:21 +0200

    changes to assert statements

--- test/isFromBellville.test.js
+++ test/isFromBellville.test.js
@@ -1,8 +1,8 @@
 describe('isFromBellville function' , function(){
     it('should return true if a car is from Bellville' , function(){
-        assert.isBoolean(isFromBellville("CY"));
+        assert.equal(true, isFromBellville("CY"));
     });
     it('should return false if a car is not from Bellville', function(){
-        assert.isBoolean(isFromBellville("CJ"));
+        assert.equal(false, isFromBellville("CJ"));
     })
 });
--- test/isWeekday.test.js
+++ test/isWeekday.test.js
@@ -1,8 +1,8 @@
 describe("isWeekday function" , function(){
     it("should return true if the day is a weeekday" , function(){
-        assert.isBoolean(isWeekday(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]));
+        assert.equal(true, isWeekday("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"));
     });
     it("should return false if the day is not a weekday", function(){
-        assert.isBoolean(isWeekday(["Saturday", "Sunday"]));
+        assert.equal(false, isWeekday("Saturday", "Sunday"));
     });
 });
